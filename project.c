c3719eea9ff040d1de58aee275476dae2897d2a9
Jake Terry <47571311+jaketerry28@users.noreply.github.com>
2025-12-08 10:16:01 -0500

    Update project.c

--- project.c
+++ project.c
@@ -2,37 +2,39 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-
-int getUserInput();
 int convertToBinary(int value);
 
-int main() {
+int main()
+{
 
     char hex;
     bool exit = false;
 
-    while (!exit) {
+    while (!exit)
+    {
         printf("Enter a hex digit (0-9, A-F) or X to exit: ");
-        scanf(" %c", &hex);   // space before %c consumes whitespace
+        scanf(" %c", &hex); // space before %c consumes whitespace
 
         hex = toupper(hex);
+        int value;
 
         if (hex == 'X')
+        {
             exit = true;
-
-        int value;
-        if (hex >= '0' && hex <= '9') {
+            continue;
+        }
+        else if (hex >= '0' && hex <= '9')
+        {
             // subtract ASCII '0' to get numeric value of hex
             value = hex - '0';
-        } 
-        else if (hex >= 'A' && hex <= 'F') {
+        }
+        else if (hex >= 'A' && hex <= 'F')
+        {
             // subtract ASCII 'A' and add 10 gets numeric value of hex
             value = hex - 'A' + 10;
-        } 
-        else if (hex == 'X') {
-            break;  // exit the loop
         }
-        else {
+        else
+        {
             printf("Invalid input.\n");
             continue;
         }
@@ -45,13 +47,14 @@ int main() {
     return 0;
 }
 
-int convertToBinary(int value) {
-        // Extract 4-bit binary using bitwise AND + shifting
-        int b3 = (value >> 3) & 1;  // most significant bit
-        int b2 = (value >> 2) & 1;
-        int b1 = (value >> 1) & 1;
-        int b0 =  value       & 1;  // least significant bit
+int convertToBinary(int value)
+{
+    // Extract 4-bit binary using bitwise AND + shifting
+    int b3 = (value >> 3) & 1; // most significant bit
+    int b2 = (value >> 2) & 1;
+    int b1 = (value >> 1) & 1;
+    int b0 = value & 1; // least significant bit
 
-        int binary = b3 * 1000 + b2 * 100 + b1 * 10 + b0;
-        return binary;
+    int binary = b3 * 1000 + b2 * 100 + b1 * 10 + b0;
+    return binary;
 }
